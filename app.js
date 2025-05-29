810e2e7df5d212a57bdcbc156a316081e6573d25
marineCorbelAFEC <marine.corbel@afec.fr>
2025-05-29 19:48:43 +0200

    passport and session

--- app.js
+++ app.js
@@ -12,37 +12,12 @@ import organizationRouter from './routes/organizationRouter.js';
 import helmet from 'helmet';
 
 import fs from 'fs';
+import session from 'express-session';
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
-// 1. Charge le JSON complet (tel que téléchargé depuis Google Cloud)
-const allSecrets = JSON.parse(fs.readFileSync(process.env.SECRET_PATH, 'utf8'));
-
-// 2. Selon le format, les credentials sont souvent sous l’objet "web"
-const creds = allSecrets.web || allSecrets.installed || allSecrets;
-
-// 3. Récupère les valeurs clés
-const GOOGLE_CLIENT_ID = creds.client_id;
-const GOOGLE_CLIENT_SECRET = creds.client_secret;
-// redirect_uris est un tableau, prends la première (celle que tu as déclarée dans Google Console)
-const GOOGLE_CALLBACK_URL = creds.redirect_uris[0];
-
-// 4. Utilisation dans Passport
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: GOOGLE_CALLBACK_URL,
-    },
-    (accessToken, refreshToken, profile, done) => {
-      done(null, profile);
-    }
-  )
-);
 
 const app = express();
-console.info('Démarrage du serveur...');
-console.log(`Port du serveur : ${process.env.SERVER_PORT}`);
+
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
@@ -67,6 +42,74 @@ app.disable('x-powered-by');
 app.use(cookieParser());
 app.use(express.json());
 
+const allSecrets = JSON.parse(fs.readFileSync(process.env.SECRET_PATH, 'utf8'));
+
+const creds = allSecrets.web || allSecrets.installed || allSecrets;
+
+const GOOGLE_CLIENT_ID = creds.client_id;
+const GOOGLE_CLIENT_SECRET = creds.client_secret;
+
+const GOOGLE_CALLBACK_URL = creds.redirect_uris[0];
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000,
+      secure: process.env.NODE_ENV === 'production',
+      httpOnly: true,
+    },
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// 4. Utilisation dans Passport
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: GOOGLE_CALLBACK_URL,
+    },
+    (accessToken, refreshToken, profile, done) => {
+      done(null, profile);
+    }
+  )
+);
+
+console.info('Démarrage du serveur...');
+console.log(`Port du serveur : ${process.env.SERVER_PORT}`);
+
+// Sérialisation/desérialisation de l'utilisateur
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+
+// Middleware de vérification d'authentification
+function ensureAuth(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect('/auth/google');
+}
+
+app.get('/', (req, res) => {
+  res.send(`accueil de l'application `);
+});
+app.get('/fail', (req, res) => {
+  res.send(`authentification échouée, veuillez réessayer`);
+});
+
+app.get('/fichier', ensureAuth, (req, res) => {
+  res.send(
+    `test de l'authentification réussie, voici votre profil : ${JSON.stringify(
+      req.user || 'aucun utilisateur connecté'
+    )}`
+  );
+});
+
 // 1) Route pour démarrer l’authentification Google
 app.get(
   '/auth/google',
@@ -79,7 +122,7 @@ app.get(
 app.get(
   '/auth/google/callback',
   passport.authenticate('google', {
-    failureRedirect: '/', // si l’auth échoue
+    failureRedirect: '/fail', // si l’auth échoue
     successRedirect: '/', // où rediriger si l’auth réussit
   })
 );
